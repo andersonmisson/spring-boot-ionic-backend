731034ba7e5de09231c5293f7207f1e77cf4c0f9
Anderson Misson <missondeveloper@gmail.com>
2019-11-22 01:46:01 -0300

    Estado e Cidade

---
+++ src/main/java/br/com/kotrix/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package br.com.kotrix.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.kotrix.cursomc.domain.Estado;
+// com REPOSITORY ele faz as alterações automaticas no Banco de Dados Categoria
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
