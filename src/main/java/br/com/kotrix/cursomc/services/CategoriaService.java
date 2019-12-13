766a73d1303474072f96c7a3924c6e464ce4983c
Anderson Misson <missondeveloper@gmail.com>
2019-12-13 00:51:59 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/br/com/kotrix/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/kotrix/cursomc/services/CategoriaService.java
@@ -3,31 +3,43 @@ package br.com.kotrix.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import br.com.kotrix.cursomc.domain.Categoria;
 import br.com.kotrix.cursomc.repositories.CategoriaRepository;
+import br.com.kotrix.cursomc.services.exception.DataIntegrityException;
 import br.com.kotrix.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não é possivel excluir uma Categoria que possui produtos");
+		}
 	}
 }
