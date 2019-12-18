9203af3dd691fec942c1ca61168fd79d5c89b02e
Anderson Misson <missondeveloper@gmail.com>
2019-12-17 23:40:25 -0300

    Validação sintatica com Bean Validation

--- src/main/java/br/com/kotrix/cursomc/dto/CategoriaDTO.java
+++ src/main/java/br/com/kotrix/cursomc/dto/CategoriaDTO.java
@@ -2,12 +2,19 @@ package br.com.kotrix.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
 import br.com.kotrix.cursomc.domain.Categoria;
 
 public class CategoriaDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
+	
+	@NotEmpty(message = "Preenchimento Obrigátorio")
+	@Length(min = 5, max = 80, message = "O tamanho deve ter entre 5 a 80 caracteres")
 	private String nome;
 
 	public CategoriaDTO() {
--- src/main/java/br/com/kotrix/cursomc/services/CategoriaService.java
+++ src/main/java/br/com/kotrix/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import br.com.kotrix.cursomc.domain.Categoria;
+import br.com.kotrix.cursomc.dto.CategoriaDTO;
 import br.com.kotrix.cursomc.repositories.CategoriaRepository;
 import br.com.kotrix.cursomc.services.exception.DataIntegrityException;
 import br.com.kotrix.cursomc.services.exception.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 
 }
