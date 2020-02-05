837c09e8efb28d76af91682eeb7e9e2737fe0fd6
Anderson Misson <missondeveloper@gmail.com>
2020-02-05 18:56:20 -0300

    Inserindo Pedido

--- src/main/java/br/com/kotrix/cursomc/domain/ItemPedido.java
+++ src/main/java/br/com/kotrix/cursomc/domain/ItemPedido.java
@@ -10,15 +10,15 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 @Entity
 public class ItemPedido implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@JsonIgnore
 	@EmbeddedId
 	private ItemPedidoPK id = new ItemPedidoPK();
-	
+
 	private Double desconto;
 	private Integer quantidade;
 	private Double preco;
-	
+
 	public ItemPedido() {
 	}
 
@@ -30,8 +30,9 @@ public class ItemPedido implements Serializable {
 		this.quantidade = quantidade;
 		this.preco = preco;
 	}
-	
-	// Usase o GET na frente, para o JSON reconhecer com um metodo a ser mostrado, agora aparece SUBTOTAL
+
+	// Usase o GET na frente, para o JSON reconhecer com um metodo a ser mostrado,
+	// agora aparece SUBTOTAL
 	public double getSubTotal() {
 		return (preco - desconto) * quantidade;
 	}
@@ -40,12 +41,19 @@ public class ItemPedido implements Serializable {
 	public Pedido getPedido() {
 		return id.getPedido();
 	}
-	
+
+	public void setPedido(Pedido pedido) {
+		id.setPedido(pedido);
+	}
 
 	public Produto getProduto() {
 		return id.getProduto();
 	}
-	
+
+	public void setProduto(Produto produto) {
+		id.setProduto(produto);
+	}
+
 	public ItemPedidoPK getId() {
 		return id;
 	}
@@ -102,5 +110,5 @@ public class ItemPedido implements Serializable {
 			return false;
 		return true;
 	}
-	
+
 }
