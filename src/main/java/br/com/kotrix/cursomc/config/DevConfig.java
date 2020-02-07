d248b913a3827ba0f70dac51b056e644a87cb242
Anderson Misson <missondeveloper@gmail.com>
2020-02-06 23:39:02 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/br/com/kotrix/cursomc/config/DevConfig.java
@@ -0,0 +1,36 @@
+package br.com.kotrix.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import br.com.kotrix.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		// Para evitar acumulo de informação no Banco de Dados
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatabase();
+		return true;
+	}
+	
+}
