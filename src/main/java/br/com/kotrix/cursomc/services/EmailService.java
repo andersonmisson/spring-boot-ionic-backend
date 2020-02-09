78fee960c64b5d9d8003ecaecc0f9bb8f7df013c
Anderson Misson <missondeveloper@gmail.com>
2020-02-09 01:33:12 -0300

    Email HTML

--- src/main/java/br/com/kotrix/cursomc/services/EmailService.java
+++ src/main/java/br/com/kotrix/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package br.com.kotrix.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import br.com.kotrix.cursomc.domain.Pedido;
@@ -7,6 +9,11 @@ import br.com.kotrix.cursomc.domain.Pedido;
 public interface EmailService {
 
 	void sendOrderConfirmationEmail(Pedido obj);
-	
+	// Para enviar um email PLANO, é essa opção SIMPLEMAILMESSAGE
 	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	// MIMEMESSAGE é para mandar um email tipo HTML
+	void sendHtmlEmail(MimeMessage msg);
+
 }
